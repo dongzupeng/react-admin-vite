cdae97a07e34aeea1d3b96c50ca1dc795943e164
dzp <2360591410@qq.com>
2022-11-19 01:54:06 +0800

    feat:样式按需引入

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 import { Button } from 'antd'
-import 'antd/dist/antd.css'
 import { WifiOutlined } from '@ant-design/icons'
 
 function App() {
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import styleImport,{ AntdResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    styleImport({
+      resolves:[
+        AntdResolve()
+      ]
+    })
+  ],
   //配置别名
   resolve: {
     alias: {
