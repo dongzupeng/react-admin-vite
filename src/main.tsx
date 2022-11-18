1f8f5100ba8044f7e44583ae986cc5824690be4a
dzp <2360591410@qq.com>
2022-11-19 00:54:37 +0800

    feat:路径别名设置

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 //样式初始化
 import 'reset-css'
 //引入全局样式
-import './assets/styles/global.scss'
+import '@/assets/styles/global.scss'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
