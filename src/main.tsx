b909841934b336893b18d279c33d6cc2f5c63535
dzp <2360591410@qq.com>
2022-11-19 00:44:36 +0800

    feat:sass install

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //样式初始化
 import 'reset-css'
+//引入全局样式
+import './assets/styles/global.scss'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
